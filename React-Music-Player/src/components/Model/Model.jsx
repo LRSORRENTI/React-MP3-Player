3d697fa54aed944331d84024705f75f14b618efd
Luke <lrsorrenti@hotmail.com>
2023-11-07 18:09:22 -0600

    added setup for React media player

---
+++ React-Music-Player/src/components/Model/Model.jsx
@@ -0,0 +1,9 @@
+const Model = ({ onClick, darkMode }) => {
+    return (
+      <button id="model" onClick={onClick}>
+        <i className={`bi ${darkMode ? 'bi-moon' : 'bi-brightness-high-fill'}`}></i>
+      </button>
+    );
+  };
+  
+  export default Model;
---
+++ React-Music-Player/src/components/Phone/Phone.jsx
@@ -0,0 +1,9 @@
+const Phone = ({ darkMode }) => {
+    return (
+      <div className={`phone ${darkMode ? 'dark' : ''}`}>
+        {/* Phone content */}
+      </div>
+    );
+  };
+
+  export default Phone;
