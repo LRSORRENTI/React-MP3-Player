6983413acfdcb7c371b0500e233e4f9abae7c8a8
Luke <lrsorrenti@hotmail.com>
2023-11-10 20:42:44 -0600

    will need to implement functionality for dragging the slider back and forth, removed old commented out code in some components

--- React-Music-Player/src/components/AudioPlayer.jsx
+++ React-Music-Player/src/components/AudioPlayer.jsx
@@ -1,30 +1,3 @@
-// import React, { useRef, useState } from 'react';
-
-// export default function AudioPlayer({ src }) {
-//     const audioRef = useRef(null);
-//     const [isPlaying, setIsPlaying] = useState(false);
-
-//     const togglePlayPause = () => {
-//         if (audioRef.current) {
-//             if (isPlaying) {
-//                 audioRef.current.pause();
-//             } else {
-//                 audioRef.current.play();
-//             }
-//             setIsPlaying(!isPlaying);
-//         }
-//     };
-
-//     return (
-//         <div>
-//             <audio ref={audioRef} src={src} />
-//             <button onClick={togglePlayPause}>
-//                 {isPlaying ? 'Pause' : 'Play'}
-//             </button>
-//         </div>
-//     );
-// }
-
 import React, { useEffect, useRef } from 'react';
 
 export default function AudioPlayer({ src, isPlaying, onTogglePlay }) {
--- React-Music-Player/src/components/TimeSlider.jsx
+++ React-Music-Player/src/components/TimeSlider.jsx
@@ -1,14 +1,3 @@
-// export default function TimeSlider() {
-//     return (
-//      <div className="time-slider">
-//                     <div className="slider-times">
-//                         <span>0:00</span>
-//                         <span>3:58</span>
-//                     </div>
-//                     <input type="range" min="1" max="100" value="0" className="slider" id="myRange" />
-//                 </div> 
-//     )
-// }
 import React, { useState, useEffect } from 'react';
 
 export default function TimeSlider({ isPlaying, totalDuration }) {
